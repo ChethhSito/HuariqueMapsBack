eb1a1706dbed7cf51027b132b68352c2b307fbde
Chethh <jdavidhuay@gmail.com>
2026-06-16 15:02:40 -0500

    Agrega modulos de autenticacion y usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HuariquesModule } from './huariques/huariques.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,6 +13,8 @@ import { HuariquesModule } from './huariques/huariques.module';
       process.env.MONGO_URI || 'mongodb://localhost:27017/huariquemap',
     ),
     HuariquesModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
